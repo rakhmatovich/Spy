1fc8a596f47cf8627bba7eb673ebda4232fc04c1
rakhmatovich <nurmuxammadrahmatov@gmail.com>
2023-08-28 15:36:30 +0500

    router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Buttons from "../component/Buttons"
+import Footer from "../component/Footer"
+import Navbar from "../component/Navbar"
+
+function Home() {
+
+  return (
+    <div className="bg-gray-900">
+      <Navbar />
+
+      <Buttons />
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
