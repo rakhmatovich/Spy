8be51ad785d2204c0e81d1af33b867233436e2bc
muxammad <zairovmuxammadjon@gmail.com>
2023-08-28 15:52:49 +0500

    muhammad

--- src/component/Buttons.jsx
+++ src/component/Buttons.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Buttons() {
     return (
-        <div className='flex flex-col gap-5 w-full h-[90vh`] items-center justify-center'>
+        <div className='flex flex-col gap-5 w-full h-[90vh] items-center justify-center'>
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-96 h-14 rounded-full grid-cols-1">
                 New game
             </button>
