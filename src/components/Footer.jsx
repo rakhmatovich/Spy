e22d010db98c7fdc000ee937f0664431bbbdd247
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2023-09-06 15:26:17 +0500

    reference

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function () {
   return (
     <div className='flex'>
-      <h1 className='text-cyan-500 hover:text-cyan-700 text-center mx-auto  '><a href="https://t.me/thisjavik" target='_blank'>Power by Javik</a></h1>
+      <h1 className='text-cyan-500 hover:text-cyan-700 text-center mx-auto'><a href="https://t.me/thisjavik" target='_blank'>Power by Sydney</a></h1>
     </div>
   )
 }
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function Game() {
   return (
-    <div className='flex h-screen bg-gradient-to-br from-gray-900 to-cyan-700  w-full h-[100vh] '>
+    <div className='flex flex-col h-screen bg-gradient-to-br from-gray-900 to-cyan-700  w-full items-center'>
       <h1 className='text-white w-full my-32  text-5xl text-center'>Выберите параметры</h1>
+
+      <div className='w-[60%] flex justify-between'>
+        <div className=''>
+          <h1 className='m-3 text-zinc-100 text-center text-2xl'>Таймер</h1>
+          <input className='bg-transparent/20 shadow-xl outline-none text-white p-3 rounded-lg' />
+        </div>
+
+        <div className=''>
+          <h1 className='m-3 text-zinc-100 text-center text-2xl'>Игроки</h1>
+          <input className='bg-transparent/20 shadow-xl outline-none text-white p-3 rounded-lg' type="text"/>
+        </div>
+      </div>
+      <div className='m-24 flex flex-col gap-5'>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-96 h-14 rounded-full  grid-cols-1'>Шпионов:</button>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-96 h-14 rounded-full  grid-cols-1'>Все локации</button>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-96 h-14 rounded-full  grid-cols-1'>Начать игру</button>
+      </div>
     </div>
   )
 }
