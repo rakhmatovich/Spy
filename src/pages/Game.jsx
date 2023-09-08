d92723ada03a52436763d9ca8997a6b0c71c601d
rakhmatovich <nurmuxammadrahmatov@gmail.com>
2023-09-08 15:17:04 +0500

    game

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import BackButton from "../components/BackButton.jsx";
 
 export default function Game() {
+    const [timer, setTimer] = useState(1)
+    const [playersCount, setPlayersCount] = useState(3)
+    const [spyCount, setSpyCount] = useState(1)
+
+    const times = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    const players = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    const [spySelect, setSpySelect] = useState([1, 2])
+
+    useEffect(() => {
+        let fillArr = []
+        for (let i = 0; i < Math.round(playersCount * 0.445); i++) {
+            fillArr.push(i + 1)
+        }
+        if (fillArr.length > 2) setSpySelect(fillArr)
+    }, [playersCount]);
+
     return (
         <div className='flex flex-col h-screen bg-gradient-to-br from-gray-900 to-cyan-700  w-full items-center'>
             <BackButton/>
@@ -11,29 +27,35 @@ export default function Game() {
             <div className='w-[60%] flex justify-between flex-col md:flex-row'>
                 <div className="flex flex-col items-center">
                     <h1 className='m-3 text-zinc-100 text-center text-2xl'>Таймер</h1>
-                    <input
-                        className='w-[250px] bg-transparent/20 shadow-xl outline-none text-white p-3 rounded-lg'
-                        type="number"
-                        placeholder="5-10 min"
-                    />
+                    <select
+                        onChange={(e) => setTimer(+e.target.value)}
+                        className="w-[250px] bg-transparent/20 shadow-xl outline-none text-white p-3 pr-5 rounded-lg">
+                        {times.map(t => (
+                            <option value={t} key={t}>{t} min</option>
+                        ))}
+                    </select>
                 </div>
                 <div className="flex flex-col items-center">
                     <h1 className='m-3 text-zinc-100 text-center text-2xl'>Игроки</h1>
-                    <input
-                        className='w-[250px] bg-transparent/20 shadow-xl outline-none text-white p-3 rounded-lg'
-                        type="number"
-                        placeholder="4-20"
-                    />
+                    <select
+                        onChange={(e) => setPlayersCount(+e.target.value)}
+                        className="w-[250px] bg-transparent/20 shadow-xl outline-none text-white p-3 pr-5 rounded-lg">
+                        {players.map(t => (
+                            <option value={t} key={t}>{t}</option>
+                        ))}
+                    </select>
                 </div>
             </div>
             <div className=' flex flex-col gap-5'>
                 <div className='flex flex-col items-center mb-8'>
                     <h1 className='m-3 text-zinc-100 text-center text-2xl'>Шпионов</h1>
-                    <input
-                        className='w-[250px] bg-transparent/20 shadow-xl outline-none text-white p-3 rounded-lg'
-                        type="number"
-                        placeholder="1-6"
-                    />
+                    <select id='select'
+                            onChange={(e) => setSpyCount(+e.target.value)}
+                            className="w-[250px] bg-transparent/20 shadow-xl outline-none text-white p-3 pr-5 rounded-lg">
+                        {spySelect.map(t => (
+                            <option value={t} key={t}>{t}</option>
+                        ))}
+                    </select>
                 </div>
 
                 <button
