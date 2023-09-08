2fd281539ff8b9a48ff68794748e218cfdbb9548
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2023-09-08 10:36:25 +0500

    reference

--- src/pages/Reference.jsx
+++ src/pages/Reference.jsx
@@ -7,10 +7,10 @@ export default function Reference() {
         <div className='flex relative pb-20 flex-col items-center bg-gradient-to-br from-gray-900 to-cyan-700 w-[100%] h-[100%]'>
             <BackButton/>
 
-            <h1 className='text-gray-300 my-8 text-center text-3xl md:text-5xl'>
+            <h1 className='text-gray-300 my-8 text-center text-3xl md:text-5xl font-bold'>
                 Об игре
             </h1>
-            <p className='text-cyan-400  y-5 text-lg mg:text-2xl w-[90%] min-w-[300px] text-justify'>
+            <p className='text-cyan-400  y-5 mg:text-2xl w-[90%] min-w-[300px] text-justify font-bold text-lg'>
                 В настольной игре «Находка для шпиона» вы сможете побывать как в шкуре шпиона некоего правительства, так
                 и идущего по его следу спецагента — контршпиона. Данная игра относится к жанру салонной игры, то есть
                 напоминает известную «Мафию», но не требует отдельного игрока под роль ведущего, играть будут все!
@@ -21,7 +21,7 @@ export default function Reference() {
             <h1 className='text-gray-300 my-8 text-center text-3xl md:text-5xl'>
                 Как играть
             </h1>
-            <p className='text-cyan-400  my-5 text-lg mg:text-2xl w-[90%] min-w-[300px] text-justify '>
+            <p className='text-cyan-400  my-5 text-lg mg:text-2xl w-[90%] min-w-[300px] text-justify font-bold'>
                 Как упоминалось выше, игра проходит в режиме разговора. Один из игроков тайно становится шпионом и его
                 задача — узнать, где же он находится. Другие игроки знают локацию, но не знают, кто же шпион — это им
                 предстоит вычислить. Как? Задавая друг другу вопросы, конечно! Танцуя таким образом вокруг да около,
@@ -30,20 +30,20 @@ export default function Reference() {
                 чтобы отвести от себя подозрения? Шпиону необходимо во время всех разговоров понять, где он находится и
                 сделать об этом заявление до того, как его раскусят!
             </p>
-            <h1 className='text-gray-300 my-8 text-center text-3xl md:text-5xl'>
+            <h1 className='text-gray-300 my-8 text-center text-3xl md:text-5xl font-bold'>
                 Кто победил?
             </h1>
-            <p className='text-cyan-400  my-5 text-lg mg:text-2xl w-[90%] min-w-[300px] text-justify'>
+            <p className='text-cyan-400  my-5 text-lg mg:text-2xl w-[90%] min-w-[300px] text-justify font-bold '>
                 По истечению таймера все игроки должны единогласно проголосовать за шпиона, тем самым вычислив его и
                 победив в партии. Если игроки ошибаются, побеждает шпион.
                 любой игрок (даже сам шпион) один раз за партию может начать голосование за любую кандидатуру и если все
                 единогласное голосуют за одного игрока, то партия заканчивается в ту или иную пользу
                 шпион может остановить игру и назвать текущую локацию — если угадал, то победа за ним.
             </p>
-            <h1 className='text-gray-300 my-8 text-center text-3xl md:text-5xl'>
+            <h1 className='text-gray-300 my-8 text-center text-3xl md:text-5xl font-bold'>
                 Суть игры
             </h1>
-            <p className='text-cyan-400 my-5 text-lg mg:text-2xl w-[90%] min-w-[300px] text-justify'>
+            <p className='text-cyan-400 my-5 text-lg mg:text-2xl w-[90%] min-w-[300px] text-justify font-bold'>
                 Найти шпиона. Как играть – каждому игроку, кроме шпиона, дается общая локация. Шпион должен догадаться,
                 где он находится, игроки – кто шпион. Для этого все по очереди задают вопросы друг другу, на которые
                 приложением отводится 5 минут (время, как и количество шпионов, можно менять).
